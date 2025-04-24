545d5e587f539eaec7306c3e54be1681587cd893
Iurii Venger <venger.yuriy@icloud.com>
2025-04-24 14:16:03 +0200

    refactor(api): streamline exchange module by dynamically generating order type methods
    - Introduced types for exchange methods and module structure.
    - Replaced hardcoded order type methods with a dynamic generation approach using `reduce` for better maintainability.

--- src/api/exchange.ts
+++ src/api/exchange.ts
@@ -4,6 +4,18 @@ import { apiClientV1 } from '../utils/apiClientFactory';
 
 import { OrderType } from '../constants';
 
+type IExchangeByOrderType = {
+  getByFromCurrency: (from_uuid: string) => Promise<API.Exchange.Exchange[]>;
+  getByToCurrency: (to_uuid: string) => Promise<API.Exchange.Exchange[]>;
+  getByOrderType: () => Promise<API.Exchange.Exchange[]>;
+};
+
+type IExchangeModule = {
+  byOrderType: {
+    [key in OrderType]: IExchangeByOrderType;
+  };
+};
+
 const createOrderTypeMethods = (orderType: OrderType) => ({
   getByFromCurrency: (from_uuid: string) =>
     apiClientV1.getRequest<API.Exchange.Exchange[]>('/exchange/', { params: { from_uuid, order_type: orderType } }),
@@ -13,14 +25,11 @@ const createOrderTypeMethods = (orderType: OrderType) => ({
     apiClientV1.getRequest<API.Exchange.Exchange[]>('/exchange/', { params: { order_type: orderType } }),
 });
 
-export const exchange = {
-  byOrderType: {
-    [OrderType.DEPOSIT_FIAT_SEPA]: createOrderTypeMethods(OrderType.DEPOSIT_FIAT_SEPA),
-    [OrderType.DEPOSIT_FIAT_SWIFT]: createOrderTypeMethods(OrderType.DEPOSIT_FIAT_SWIFT),
-    [OrderType.WITHDRAWAL_FIAT_SEPA]: createOrderTypeMethods(OrderType.WITHDRAWAL_FIAT_SEPA),
-    // [OrderType.TRANSFER_CARD_PREPAID]: createOrderTypeMethods(OrderType.TRANSFER_CARD_PREPAID), // DO not used
-    [OrderType.TRANSFER_CARD_SUBACCOUNT]: createOrderTypeMethods(OrderType.TRANSFER_CARD_SUBACCOUNT),
-    [OrderType.WITHDRAWAL_CRYPTO]: createOrderTypeMethods(OrderType.WITHDRAWAL_CRYPTO),
-    [OrderType.EXCHANGE_CRYPTO_INTERNAL]: createOrderTypeMethods(OrderType.EXCHANGE_CRYPTO_INTERNAL),
-  },
+const orderTypes = Object.values(OrderType);
+
+export const exchange: IExchangeModule = {
+  byOrderType: orderTypes.reduce((acc, orderType) => {
+    acc[orderType] = createOrderTypeMethods(orderType);
+    return acc;
+  }, {} as Record<OrderType, IExchangeByOrderType>),
 };
