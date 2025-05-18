f9b9b646d2505e514894159fe273502be661f278
Iurii Venger <venger.yuriy@icloud.com>
2025-05-18 23:49:06 +0200

    fix(api): update endpoint paths for currencies and chains to include '/system' prefix

--- src/api/list.ts
+++ src/api/list.ts
@@ -8,10 +8,11 @@ import { apiClientV2 } from '../utils/apiClientFactory';
 
 export const list = {
   currencies: {
-    getAll: (params?: AxiosRequestConfig) => apiClientV2.getRequest<API.Currencies.CurrencyList>('/currencies', params),
+    getAll: (params?: AxiosRequestConfig) =>
+      apiClientV2.getRequest<API.Currencies.CurrencyList>('/system/currencies', params),
   },
   chains: {
-    getAll: (params?: AxiosRequestConfig) => apiClientV2.getRequest<API.Chains.ChainList>('/chains', params),
+    getAll: (params?: AxiosRequestConfig) => apiClientV2.getRequest<API.Chains.ChainList>('/system/chains', params),
   },
   countries: {
     getAll: (params?: AxiosRequestConfig) =>
