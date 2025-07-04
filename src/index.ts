67b42bebcfea79d410c98b8532c4ab32c8f84957
Iurii Venger <venger.yuriy@icloud.com>
2025-07-04 23:27:45 +0800

    feat(index): export hooks from the hooks module for improved functionality

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ export { squarefi_bff_api_client } from './api';
 export * from './utils/apiClientFactory';
 export * from './utils/tokensFactory';
 export * from './constants';
-
+export * from './hooks';
 // Also export types if you have any
 export * from './api/types/types';
