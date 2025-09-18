49e8f01de96455becb14ea83eb45752d44625718
Iurii <venger.yuriy@icloud.com>
2025-09-18 12:48:34 +0200

    refactor(api): update bank data retrieval method to use GET with params for improved clarity and consistency

--- src/api/bank-data.ts
+++ src/api/bank-data.ts
@@ -2,12 +2,10 @@ import { apiClientV2 } from '../utils/apiClientFactory';
 import { API } from './types/types';
 
 export const bankData = {
-  getBankDataByAccountNumber: ({
-    account_number,
-  }: API.BankData.GetBankDataByAccountNumber.Request): Promise<API.BankData.GetBankDataByAccountNumber.Response> =>
-    apiClientV2.postRequest<API.BankData.GetBankDataByAccountNumber.Response>(`/bank-data`, {
-      data: {
-        account_number,
-      },
+  getBankDataByAccountNumber: (
+    params: API.BankData.GetBankDataByAccountNumber.Request
+  ): Promise<API.BankData.GetBankDataByAccountNumber.Response> =>
+    apiClientV2.getRequest<API.BankData.GetBankDataByAccountNumber.Response>(`/bank-data`, {
+      params,
     }),
 };
