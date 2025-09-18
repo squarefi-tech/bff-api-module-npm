80e59987e9d1a57d319b63ab189edc48cf474a9c
Iurii <venger.yuriy@icloud.com>
2025-09-18 12:45:48 +0200

    fix(api): change bank data retrieval method from GET to POST for improved data handling

--- src/api/bank-data.ts
+++ src/api/bank-data.ts
@@ -5,5 +5,9 @@ export const bankData = {
   getBankDataByAccountNumber: ({
     account_number,
   }: API.BankData.GetBankDataByAccountNumber.Request): Promise<API.BankData.GetBankDataByAccountNumber.Response> =>
-    apiClientV2.getRequest<API.BankData.GetBankDataByAccountNumber.Response>(`/bank-data/${account_number}`),
+    apiClientV2.postRequest<API.BankData.GetBankDataByAccountNumber.Response>(`/bank-data`, {
+      data: {
+        account_number,
+      },
+    }),
 };
